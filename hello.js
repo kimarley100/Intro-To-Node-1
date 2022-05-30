114823d83556571562cbf10bf43480eb9492138c
Kimmarley Walters <kimarley100@gmail.com>
2022-05-30 15:52:24 -0500

    Initial Commit

---
+++ hello.js
@@ -0,0 +1,21 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+
+http.createServer(function(req,res){
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    if (filename =='./'){filename = './index';}
+    filename = filename +".html";
+    fs.readFile(filename, function(err,data){
+        if(err){
+            res.writeHead(404,{'Content-Type':'text/html'});
+            return res.end("404 Not Found!")
+        }
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.write(data);
+    return res.end();
+    });
+}).listen(4041);
+
+console.log("Server listening on Port 4041...");
